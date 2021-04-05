158d45ed2497f293e3218eaf1a7db3d4909d1b53
Christine <huangc909@gmail.com>
2021-04-04 23:23:32 -0400

    Redirect sign in to home-page

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -33,7 +33,7 @@ class SignIn extends Component {
         message: messages.signInSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/'))
+      .then(() => history.push('/home-page'))
       .catch(error => {
         this.setState({ username: '', password: '' })
         msgAlert({
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -36,7 +36,7 @@ class SignUp extends Component {
         message: messages.signUpSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/'))
+      .then(() => history.push('/home-page'))
       .catch(error => {
         this.setState({ email: '', username: '', password: '', passwordConfirmation: '' })
         msgAlert({
